c5339d6521fe80e9676f3bb0c084aca9c38bcc6b
authorbeard <msnolan@gmail.com>
2016-06-01 09:14:35 -0400

    done

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,29 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favFood
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		favFood = $filter('favoriteFood');
 	}));
+
+  it('should filter the list of contacts by their favorite foods', function(){
+    var people = [
+      {
+        name: 'jim"',
+        favoriteFood: 'deez', 
+      },{
+        name: 'jenny', 
+        favoriteFood: 'nuts' 
+      }, {
+        name: 'bill',
+        favoriteFood: 'burritos'
+      }
+      ]
+
+    expect(favFood(people, 'nuts')[0].name).toEqual('jenny')
+
+  })
 
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,16 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, remVowels
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		remVowels = $filter('removeAllVowels');
 	}));
+
+  it('should remove all the vowels from a word', function(){
+
+    expect(remVowels('Stevie')).toEqual('Stv')
+
+  })
 
 });
